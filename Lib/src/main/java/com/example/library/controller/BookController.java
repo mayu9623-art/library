1464f99eaf3343fab8b3090c98f3a34305de01da
mayu9623-art <mayu9623@gmail.com>
2026-07-19 16:00:33 +0900

    Update BookController.java

--- Lib/src/main/java/com/example/library/controller/BookController.java
+++ Lib/src/main/java/com/example/library/controller/BookController.java
@@ -15,7 +15,7 @@ import com.example.library.dto.BookUpdateRequest;
 import com.example.library.service.BookService;
 
 import lombok.RequiredArgsConstructor;
-import　org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/books")
